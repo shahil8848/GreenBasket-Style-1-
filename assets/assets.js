25a733b5f2da6fd00edec5455ece3bdbd4d3908d
shahil8848 <shahilbhusal2002@gmail.com>
2025-07-08 01:05:47 +0545

    Initial clean push

---
+++ assets/assets.js
@@ -0,0 +1,136 @@
+import logo from "./logo.png";
+import search_icon from "./search_icon.svg";
+import user_icon from "./user_icon.svg";
+import cart_icon from "./cart_icon.svg";
+import add_icon from "./add_icon.svg";
+import order_icon from "./order_icon.svg";
+import instagram_icon from "./instagram_icon.svg";
+import facebook_icon from "./facebook_icon.svg";
+import twitter_icon from "./twitter_icon.svg";
+import box_icon from "./box_icon.svg";
+import product_list_icon from "./product_list_icon.svg";
+import menu_icon from "./menu_icon.svg";
+import arrow_icon from "./arrow_icon.svg";
+import increase_arrow from "./increase_arrow.svg";
+import decrease_arrow from "./decrease_arrow.svg";
+import arrow_right_icon_colored from "./arrow_right_icon_colored.svg";
+import my_location_image from "./my_location_image.svg";
+import arrow_icon_white from "./arrow_icon_white.svg";
+import heart_icon from "./heart_icon.svg";
+import star_icon from "./star_icon.svg";
+import redirect_icon from "./redirect_icon.svg";
+import star_dull_icon from "./star_dull_icon.svg";
+import checkmark from "./checkmark.png";
+import upload_area from "./upload_area.png";
+import banner_1 from "./banner.png";
+import banner_2 from "./banner33333.png";
+import banner_3 from "./header_hempbag_1.png";
+import girl_withbag from "./girl_with_bag-removebg-preview.png";
+import girl_withtiffin from "./tiffinbox.png";
+import hempoil from "./banner.png";
+import bamboostraw_image from "./bamboostraw.png";
+import beeswax from "./Beeswax_Food_Wraps-.png";
+import ecospoon_image from "./ecospoon.png";
+import bottle_image from "./bottle.png";
+import bambooracks_image from "./bambooracks.png";
+import bag1_image from "./bag1.png";
+
+import bag31_image from "./bag31.png";
+import ecodinnerset_image from "./ecodinnerset.png";
+import mudcooking_image from "./mudcooking.png";
+import mudutensils_image from "./mudutensils.png";
+import bag_image from "./bag1.png";
+import hempoil_image from "./hempoill.png";
+
+
+import product_details_bag from "./bag2.png";
+import product_details_bag21 from "./bag21.png";
+import product_details_bag22 from "./bag22.png";
+import product_details_bag2 from "./bag2.png";
+
+export const assets = {
+  logo,
+  search_icon,
+  user_icon,
+  cart_icon,
+  add_icon,
+  order_icon,
+  instagram_icon,
+  facebook_icon,
+  twitter_icon,
+  box_icon,
+  product_list_icon,
+  menu_icon,
+  arrow_icon,
+  increase_arrow,
+  decrease_arrow,
+  arrow_right_icon_colored,
+  my_location_image,
+  arrow_icon_white,
+  heart_icon,
+  star_icon,
+  redirect_icon,
+  star_dull_icon,
+  banner_1,
+  banner_2,
+  banner_3,
+  upload_area,
+  checkmark,
+  girl_withbag,
+  girl_withtiffin,
+  hempoil,
+  bamboostraw_image,
+  beeswax ,
+
+  ecospoon_image,
+  bottle_image,
+  bambooracks_image,
+  bag1_image ,
+  bag31_image,
+  ecodinnerset_image,
+  mudcooking_image,
+  mudutensils_image,
+
+
+  bag_image,
+  hempoil_image,
+  
+  product_details_bag ,
+  product_details_bag21 ,
+  product_details_bag22 ,
+  product_details_bag2 ,
+  
+  
+};
+
+export const BagIcon = () => {
+  return (
+    <svg className="w-5 h-5 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M9 10V6a3 3 0 0 1 3-3v0a3 3 0 0 1 3 3v4m3-2 .917 11.923A1 1 0 0 1 17.92 21H6.08a1 1 0 0 1-.997-1.077L6 8h12Z" />
+    </svg>
+  )
+}
+
+export const CartIcon = () => {
+  return (
+    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0.75 0.75H3.75L5.76 10.7925C5.82858 11.1378 6.01643 11.448 6.29066 11.6687C6.56489 11.8895 6.90802 12.0067 7.26 12H14.55C14.902 12.0067 15.2451 11.8895 15.5193 11.6687C15.7936 11.448 15.9814 11.1378 16.05 10.7925L17.25 4.5H4.5M7.5 15.75C7.5 16.1642 7.16421 16.5 6.75 16.5C6.33579 16.5 6 16.1642 6 15.75C6 15.3358 6.33579 15 6.75 15C7.16421 15 7.5 15.3358 7.5 15.75ZM15.75 15.75C15.75 16.1642 15.4142 16.5 15 16.5C14.5858 16.5 14.25 16.1642 14.25 15.75C14.25 15.3358 14.5858 15 15 15C15.4142 15 15.75 15.3358 15.75 15.75Z" stroke="#4b5563" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <defs>
+        <rect width="18" height="18" fill="white" />
+      </defs>
+    </svg>
+
+  )
+}
+
+export const BoxIcon = () => (
+  <svg className="w-5 h-5 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M10 21v-9m3-4H7.5a2.5 2.5 0 1 1 0-5c1.5 0 2.875 1.25 3.875 2.5M14 21v-9m-9 0h14v8a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-8ZM4 8h16a1 1 0 0 1 1 1v3H3V9a1 1 0 0 1 1-1Zm12.155-5c-3 0-5.5 5-5.5 5h5.5a2.5 2.5 0 0 0 0-5Z" />
+  </svg>
+);
+
+export const HomeIcon = () => (
+  <svg className="w-5 h-5 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" >
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5" />
+  </svg>
+);
